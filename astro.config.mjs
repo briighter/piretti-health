8d26f52b887289788a269887a93059122a91435f
Jason Lima <jaylima0@gmail.com>
2026-04-13 20:46:30 -0400

    feat: add GitHub Actions workflow for deployment to GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,8 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
+  site: 'https://briighter.github.io',
+  base: '/piretti-health',
   integrations: [tailwind()],
   output: 'static',
 });
